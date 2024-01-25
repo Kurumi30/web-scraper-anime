9bb7349e808f6356fb5e91400949e97a3e1c8ff2
Kurumi30 <bsd3su@gmail.com>
2024-01-24 23:00:10 -0300

    wip

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
-import { accessDownloadPage } from "./lib/index.js"
+import { getEpisodeInfo } from "./lib/index.js"
+import { fetcher } from "./lib/utils.js"
 
 const app = express()
 const port = process.env.PORT || 5050
@@ -10,27 +11,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set("json spaces", 2)
 
-app.get("/", async (req, res) => {
-  res.status(200).json(await accessDownloadPage())
-})
+app.get("/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-3rd-season", async (req, res) => {
+  const anime = "youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-3rd-season"
+  const { data, ...response } = await getEpisodeInfo(anime)
+  const result = await Promise.all(data[0].map(async item => {
+    const request = await fetcher(item.episode)
 
-app.get("/html", (req, res) => {
-  res.status(200).send(
-    `<!DOCTYPE html>
-    <html lang="pt-BR">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Video</title>
-    </head>
-    <body>
-      <video controls>
-        <source src="https://saikoanimes.net/download?url=VmFzY29odHRwczovL2Rvd25sb2FkLnNhaWtvYW5pbWVzLm5ldC9hcGkvdjEvZmlsZS1lbnRyaWVzL2Rvd25sb2FkL01UZ3hNek0yZkhCaFpBP3NoYXJlYWJsZV9saW5rPTkyNjgmcGFzc3dvcmQ9bnVsbA%3D%3D&timestamp=1706111206" type="video/mp4">
-        Your browser does not support the video tag
-      </video>
-    </body>
-    </html>`
-  )
+    return request.data
+  }))
+
+  res.status(200).json({ response, result })
 })
 
 app.use((req, res) => {
